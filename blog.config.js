7b57787d2e834938dce1b6f73fb338881c82d89b
lianyazhou <48372914+lianyazhou@users.noreply.github.com>
2021-05-26 09:22:28 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -16,7 +16,7 @@ const BLOG = {
   showAbout: true, // WIP
   showArchive: true, // WIP
   autoCollapsedNavBar: false, // the automatically collapsed navigation bar
-  socialLink: 'https://linktr.ee/admin',
+  socialLink: 'https://linktr.ee/lanjoy',
   seo: {
     keywords: ['Blog', 'Website', 'Notion'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
